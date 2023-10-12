18d7ebe11022c42f1816e578593ae0f0b2cac9f4
kush Gupta <kushagra@rapidinnovation.dev>
2023-10-12 09:32:24 +0530

    first_push

---
+++ app/middlewares/authJwt.js
@@ -0,0 +1,66 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth.config.js");
+const db = require("../models");
+const User = db.user;
+const Role = db.role;
+
+verifyToken = (req, res, next) => {
+  let token = req.headers["x-access-token"];
+
+  if (!token) {
+    return res.status(403).send({ message: "No token provided!" });
+  }
+
+  jwt.verify(token, config.secret, async (err, decoded) => {
+    if (err) {
+      return res.status(401).send({
+        message: "Unauthorized!",
+      });
+    }
+    req.userId = decoded.id;
+    next();
+  });
+};
+
+isAdmin = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.userId);
+    const roles = await Role.find({ _id: { $in: user.roles } });
+
+    for (let i = 0; i < roles.length; i++) {
+      if (roles[i].name === "admin") {
+        next();
+        return;
+      }
+    }
+
+    res.status(403).send({ message: "Require Admin Role!" });
+  } catch (err) {
+    res.status(500).send({ message: err });
+  }
+};
+
+isModerator = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.userId);
+    const roles = await Role.find({ _id: { $in: user.roles } });
+
+    for (let i = 0; i < roles.length; i++) {
+      if (roles[i].name === "moderator") {
+        next();
+        return;
+      }
+    }
+
+    res.status(403).send({ message: "Require Moderator Role!" });
+  } catch (err) {
+    res.status(500).send({ message: err });
+  }
+};
+
+const authJwt = {
+  verifyToken,
+  isAdmin,
+  isModerator,
+};
+module.exports = authJwt;
